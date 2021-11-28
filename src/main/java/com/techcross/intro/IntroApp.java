20353d18e2830817bca9764ae3ff4d64df31e37f
shubhamAsolkar22 <shubham.asolkar21@gmail.com>
2021-11-28 22:44:26 +0530

    Retrieving Objects using session.get

--- src/main/java/com/techcross/intro/IntroApp.java
+++ src/main/java/com/techcross/intro/IntroApp.java
@@ -9,19 +9,39 @@ import com.techcross.util.HibernateUtils;
 public class IntroApp {
 	public static void main(String[] args) {
 		UserDetails userDetails = new UserDetails();
+
 		userDetails.setUserId(5);
+		
 		userDetails.setUserName("Shubham3");
+		
 		userDetails.setJoiningDate(new Date());
+		
 		userDetails.setAddress("Heaven");
+		
 		userDetails.setTransientProperty("sdasds");
-//		Open session -> begin transaction -> do something with entities -> commit transaction
+
+		//		Open session -> begin transaction -> do something with entities -> commit transaction
+		
 		Session session = HibernateUtils.getSessionFactory().openSession();
-		
+
 		session.beginTransaction();
-		
+
 		session.save(userDetails);
+
+		session.getTransaction().commit();
+
+		session.close();
+
+		session = HibernateUtils.getSessionFactory().openSession();
+
+		session.beginTransaction();
+
+		UserDetails ud = session.get(UserDetails.class, 5);
 		
 		session.getTransaction().commit();
 
+		session.close();
+		
+		System.out.println(ud.getUserName());
 	}
 }
