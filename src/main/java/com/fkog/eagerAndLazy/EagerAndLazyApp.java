e951bea860121164b91b7ba5eaf58e95d1257f59
shubhamAsolkar22 <shubham.asolkar21@gmail.com>
2021-12-03 20:12:08 +0530

    updates for lazy and eager initialization.

--- src/main/java/com/fkog/eagerAndLazy/EagerAndLazyApp.java
+++ src/main/java/com/fkog/eagerAndLazy/EagerAndLazyApp.java
@@ -1,4 +1,4 @@
-package com.fkog.savingCollections;
+package com.fkog.eagerAndLazy;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -7,11 +7,11 @@ import org.hibernate.Session;
 
 import com.fkog.util.HibernateUtils;
 
-public class SavingCollectionApps {
+public class EagerAndLazyApp {
 	public static void main(String[] args) {
 
-		UserDetails5 ud5 = new UserDetails5();
-		ud5.setUserName("Shubham_ud3");
+		UserDetails6 ud6 = new UserDetails6();
+		ud6.setUserName("Shubham_ud3");
 
 		Address add1 = new Address();
 		add1.setCity("Mumbai");
@@ -29,16 +29,27 @@ public class SavingCollectionApps {
 		addressSet.add(add1);
 		addressSet.add(add2);
 
-		ud5.setAddresses(addressSet);
+		ud6.setAddresses(addressSet);
 
 		Session session = HibernateUtils.getSessionFactory().openSession();
 		session.beginTransaction();
 
-		session.save(ud5);
+		session.save(ud6);
 
 		session.getTransaction().commit();
 
 		session.close();
-
+		
+		ud6 = null;
+		
+		session = HibernateUtils.getSessionFactory().openSession();
+		ud6 = session.get(UserDetails6.class, 1);
+		//addresses not fetched
+		//only proxy userdetails object without adresses
+		session.close(); //will prevent adresses from  being fetched if fetch type is lazy.
+		System.out.println(ud6.getAddresses().size());;
+		//now it runs query addresses
+		//this is lazy initialization.
+		
 	}
 }
---
+++ src/main/java/com/fkog/eagerAndLazy/UserDetails6.java
@@ -0,0 +1,61 @@
+package com.fkog.eagerAndLazy;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "TBL_USER_DETAILS_6")
+public class UserDetails6 {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+
+	private String userName;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@JoinTable(name = "TBL_USER_ADDRESSES", joinColumns = @JoinColumn(name = "USER_ID")
+	)
+	private Collection<Address> addresses = new ArrayList<>();
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Collection<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(Collection<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+}
